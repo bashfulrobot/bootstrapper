753c5cdcfb85610c22b5b68d9149c1f0d6241404
bashfulrobot <dustin@bashfulrobot.com>
2020-12-07 09:27:09 -0800

    ✨ Added clone steps
    - I will now be able to run from an URL

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,10 @@
 
 sudo apt install ansible git -y
 
+cd /tmp
+git clone https://github.com/bashfulrobot/bootstrapper.git
+cd bootstrapper
+
 ansible-playbook --ask-vault-pass bootstrap.yaml
 
 exit 0
