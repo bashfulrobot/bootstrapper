a36098832712043d4e9eabba99c854bc3ff5d0b9
bashfulrobot <dustin@bashfulrobot.com>
2020-12-07 10:55:28 -0800

    🎨 clean up the shell script
    - make it portable
    - add comments
    - clean up the working directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-sudo apt install ansible git -y
+# # # # # CONFIG
 
-cd /tmp
-git clone https://github.com/bashfulrobot/bootstrapper.git
-cd bootstrapper
+# binaries
+WHICH="/usr/bin/which"
+SUDO="$($WHICH sudo)"
+APT="$($WHICH apt)"
+GIT="$($WHICH git)"
+AV="$($WHICH ansible-playbook)"
+RM="$($WHICH rm)"
 
-ansible-playbook --ask-vault-pass bootstrap.yaml
+# locations
+WORKING="/tmp"
+REPO="${WORKING}/bootstrapper"
+REPO_URL="https://github.com/bashfulrobot/bootstrapper.git"
+
+# # # # # MAIN
+
+# install dependencies
+${SUDO} ${APT} install ansible git -y
+
+# throwaway location
+cd ${WORKING}
+${GIT} clone ${REPO_URL}
+cd ${REPO}
+
+# run the bootstrap
+${AV} --ask-vault-pass bootstrap.yaml
+
+# cleanup
+cd
+${RM} -rf ${REPO}
 
 exit 0
